c436730b27bbe3f4fdfbaa75d7a64b01e9876204
guyish1 <piercewlangford@outlook.com>
2020-01-27 17:17:53 -0500

    wrote drive train subsystem

---
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -0,0 +1,48 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import edu.wpi.first.wpilibj.SpeedController;
+
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+
+
+public class DriveTrainSubsystem extends SubsystemBase {
+  
+
+  private MecanumDrive driveTrain;
+
+  private SpeedController backRightMotor;
+  private SpeedController backLeftMotor;
+  private SpeedController frontRightMotor;
+  private SpeedController frontLeftMotor;
+  
+  /**
+   * Creates a new DriveTrainSubsystem.
+   */
+  public DriveTrainSubsystem(SpeedController backRightMotor, SpeedController backLeftMotor, SpeedController frontRightMotor, SpeedController frontLeftMotor) {
+    this.backRightMotor = backRightMotor;
+    this.backLeftMotor = backLeftMotor;
+    this.frontLeftMotor = frontLeftMotor;
+    this.frontRightMotor = frontRightMotor;
+  
+    this.driveTrain = new MecanumDrive(frontLeftMotor, backLeftMotor, frontRightMotor, backRightMotor);
+  
+  }
+
+  public void drive(double ySpeed, double xSpeed, double zRotation) {
+    driveTrain.driveCartesian(ySpeed, xSpeed, zRotation);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
