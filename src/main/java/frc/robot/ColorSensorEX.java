a6ab710700f9ae361b0081b30534d8bb8fac53d3
Dimitra <45525129+DimitriaPowell@users.noreply.github.com>
2020-01-29 16:34:30 -0500

    Update ColorSensorEX.java

--- src/main/java/frc/robot/ColorSensorEX.java
+++ src/main/java/frc/robot/ColorSensorEX.java
@@ -1,5 +1,8 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* This is example code from Rev Robotics. It is for the rev robotics color   
+/* sensor V3
+/*  
+/*Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
