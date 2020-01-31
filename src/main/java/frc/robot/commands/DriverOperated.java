fb0305f58716e5008962504be19df75bff8e2ba5
Isaiah Kahler <isaiahwkahler@gmail.com>
2020-01-30 19:07:06 -0500

    proper driver operated command (from scratch)

---
+++ src/main/java/frc/robot/commands/DriverOperated.java
@@ -0,0 +1,54 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import frc.robot.subsystems.DriveTrainSubsystem;
+import edu.wpi.first.wpilibj.Joystick;
+
+public class DriverOperated extends CommandBase {
+
+  DriveTrainSubsystem driveTrain;
+  Joystick controller1 = new Joystick(0);
+  /**
+   * Creates a new DriverOperated.
+   */
+  public DriverOperated(DriveTrainSubsystem driveTrain) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.driveTrain = driveTrain;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+
+    double ySpeed = controller1.getRawAxis(5);
+    double xSpeed = controller1.getRawAxis(4);
+    double zRotation = controller1.getRawAxis(0);
+
+    driveTrain.drive(ySpeed, xSpeed, zRotation);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    driveTrain.drive(0, 0, 0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
