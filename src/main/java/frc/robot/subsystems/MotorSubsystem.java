88c358b6541be41a6f208a751987db14f35bbc86
Isaiah Kahler <isaiahwkahler@gmail.com>
2020-02-14 08:51:25 -0500

    curve power and scalar power on motorsubsystem

--- src/main/java/frc/robot/subsystems/MotorSubsystem.java
+++ src/main/java/frc/robot/subsystems/MotorSubsystem.java
@@ -14,13 +14,39 @@ import edu.wpi.first.wpilibj.SpeedController;
 public class MotorSubsystem extends SubsystemBase {
   
   private SpeedController motor;
+  private double scalarPower = 1.0;
 
+    /**
+   * Basic motor power subsystem.
+   * @param motor - SpeedController object to control motor
+   */
   public MotorSubsystem(SpeedController motor) {
     this.motor = motor;
   }
 
+  /**
+   * Basic motor power subsystem.
+   * @param motor - SpeedController object to control motor
+   * @param scalarPower - limit power by giving a decimal value between 0 and 1. 
+   */
+  public MotorSubsystem(SpeedController motor, double scalarPower) {
+    this.motor = motor;
+    this.scalarPower = scalarPower;
+  }
+
   public void powerMotor(double speed) {
-    motor.set(speed);
+    motor.set(speed * scalarPower);
+  }
+
+  /**
+   * Models input power to the curve of power^(1/3) for inputs with dead zones close to zero. 
+   */
+  public void curvePowerMotor(double power){
+    double adjustedPower = Math.pow(Math.abs(power), (1.0 / 3.0));
+    if (power < 0) {
+        adjustedPower *= -1;
+    }
+    this.motor.set(adjustedPower);
   }
 
   @Override
