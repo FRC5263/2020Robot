ded00ea21a4be8855766f53593849fa7e02ded8e
guyish1 <piercewlangford@outlook.com>
2020-02-20 17:47:52 -0500

    added the start of intake code

--- src/main/java/frc/robot/commands/DriverOperated.java
+++ src/main/java/frc/robot/commands/DriverOperated.java
@@ -8,24 +8,29 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import frc.robot.subsystems.DriveTrainSubsystem;
 import frc.robot.subsystems.MotorSubsystem;
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 
 public class DriverOperated extends CommandBase {
 
   DriveTrainSubsystem driveTrain;
   MotorSubsystem conveyor;
+  MotorSubsystem intake;
   Joystick controller1 = new Joystick(0);
+  Joystick controller2 = new Joystick(1);
+
 
   /**
    * Creates a new DriverOperated.
    */
-  public DriverOperated(DriveTrainSubsystem driveTrain, MotorSubsystem conveyor) {
+  public DriverOperated(DriveTrainSubsystem driveTrain, MotorSubsystem conveyor, MotorSubsystem intake) {
     // Use addRequirements() here to declare subsystem dependencies.
     this.driveTrain = driveTrain;
     this.conveyor = conveyor;
+    this.intake = intake;
   }
 
   // Called when the command is initially scheduled.
@@ -36,12 +41,13 @@ public class DriverOperated extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-
-    double forwardSpeed = -controller1.getRawAxis(5);
+    double forwardSpeed = controller1.getRawAxis(5);
     double horizontalSpeed = controller1.getRawAxis(4);
     double rotation = controller1.getRawAxis(0);
-
     driveTrain.drive(horizontalSpeed, forwardSpeed, rotation);
+
+    //  dont know how we are using controller so fix that soon "double intakeSpeed = controller2.getRawAxis();""
+    intake.powerMotor(intakeSpeed);
 
     int dpadDirection = controller1.getPOV();
     if (dpadDirection == 0) {
